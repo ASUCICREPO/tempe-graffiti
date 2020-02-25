ad18b953f9bd799374f9bf98a4325ae77360a2a6
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2020-02-25 16:53:05 -0700

    Created and tested new model's confidence interval

--- localRun.py
+++ localRun.py
@@ -3,31 +3,39 @@ import matplotlib.pyplot as plot
 import cv2
 import numpy as np
 from mxnet.io import DataBatch
+import time
 
-net = mx.mod.Module.load('./image-classification', 1)
-image_l = 64
-image_w = 64
+net = mx.mod.Module.load('./image-classification', 8)
+image_l = 256
+image_w = 256
 net.bind(for_training=False, data_shapes=[('data', (1, 3, image_l, image_w))], label_shapes=net._label_shapes)
 
 
 def get_image(url, show=False):
     # download and show the image
-    fname = mx.test_utils.download(url)
+    try:
+        fname = mx.test_utils.download(url)
+    except:
+        fname = mx.test_utils.download(fname=url)
     img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
     if img is None:
         return None
     if show:
         plot.imshow(img)
         plot.axis('off')
+        plot.show()
     # convert into format (batch, RGB, width, height)
-    img = cv2.resize(img, (64, 64))
+    img = cv2.resize(img, (256, 256))
     img = np.swapaxes(img, 0, 2)
+    # cv2.imwrite('compressed.png', img)
     img = np.swapaxes(img, 1, 2)
     img = img[np.newaxis, :]
     return img
 
 
 def predict(url, labels):
+    start_time = time.time()
+
     img = get_image(url, show=True)
     # compute the predict probabilities
     net.forward(DataBatch([mx.nd.array(img)]))
@@ -38,12 +46,21 @@ def predict(url, labels):
     prob = np.squeeze(prob)
     a = np.argsort(prob)[::-1]
 
+    print("Total size of the features = %s" %(len(a)))
     for i in a[0:5]:
-        print('probability=%f, i=%s' % (prob[i], i))
-        # print('probability=%f, class=%s' % (prob[i], labels[i]))
+        print('probability=%f, label=%s' % (prob[i], labels[i]))
 
-    print('probability of non-graffiti=%f, i=%s' % (prob[1], 1))
+    # print('probability of graffiti=%f, i=%s' % (prob[0], labels[0]))
+    # print('probability of non-graffiti=%f, i=%s' % (prob[1], labels[1]))
+    end_time = time.time()
+    print("Total execution time: {} seconds".format(end_time - start_time))
 
 
 labels = ['graffiti', 'not_graffiti']
 predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/GraffitiFeature.jpg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/download.jpeg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/Not_Graffiti_1.jpg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/Not_Graffiti_2.jpg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/2014-17-12-KORN1-e1418807468822.jpg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/15747100163591ad831fff30b2280ce04676c7f05c.jpg', labels)
+predict('https://sagemaker-graffiti-images.s3.amazonaws.com/001.graffiti/7ae4ba68596667.5b625681347a6.jpg', labels)
