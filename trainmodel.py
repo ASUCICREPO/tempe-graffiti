cc21e290cb6fa11c45ac7cb4641a9516188d5bb0
Shantanu Gupta <shantanu.smg@gmail.com>
2020-04-13 22:24:25 -0700

    split only images

--- trainmodel.py
+++ trainmodel.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import random
 import subprocess
-
+import glob
 train_ratio = 0.8
 batch_size = 32
 max_batch = 300
@@ -34,9 +34,11 @@ def split_data():
         os.makedirs(os.path.join(test_dir,_class))
 
         image_dir = os.path.join(dataset_dir, _class)
-        image_set = set(os.listdir(image_dir))
+        # Only found jpg images in given set 
+        image_set = set(glob.glob(os.path.join(image_dir, "*.jpg")))
+        # print("\nImage Set",len(image_set))
         train_set = set(random.sample(image_set, min(findNum(len(image_set),batch_size),max_batch*batch_size//2)))
-        print(len(train_set))
+        # print(len(train_set))
         # train_set = random.sample(image_set, int(train_ratio*len(image_set)))
         # train_set = set(train_set[: ((len(train_set) - (len(train_set) % batch_size) )+1)])
         remaining_set = image_set - train_set
@@ -59,14 +61,14 @@ def create_rec_file():
         rec_dir = os.path.join(proj_dir,"rec")
         rec_file = os.path.abspath("im2rec.py")
         lst_file = os.path.join(rec_dir,"tempe-graffiti-"+rec_type)
-        print("\nRun cmd for %s lst file"%rec_type)
+        print("\nRunning cmd for %s lst file"%rec_type)
         command = r"python {0} {1} {2} --recursive --list --num-thread 8".format(rec_file, lst_file,  train_dir if rec_type=='train' else validation_dir)
         print(command)
         proc = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
         out,err = proc.communicate()
         print("\nOutput",out)
         print("\nError",err)
-        print("\nRun cmd for %s rec file"%rec_type)
+        print("\nRunning cmd for %s rec file"%rec_type)
         command = r"python {0} {1} {2} --recursive --pass-through --pack-label --num-thread 8".format(rec_file, lst_file, train_dir if rec_type=='train' else validation_dir)
         print(command)
         proc = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
@@ -74,9 +76,11 @@ def create_rec_file():
         print("\nOutput",out)
         print("\nError",err)
 
+def upload_rec_to_s3():
     print("\n\nExecute pwershell script to upload rec files")
     subprocess.run(["powershell.exe", "C:\\cic\\tempe-graffiti\\upload_rec.ps1"])
     print("\nExecuted pwershell script to upload rec files")
         
 split_data()
 create_rec_file()
+upload_rec_to_s3()
