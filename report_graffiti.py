91e273d3146f6a78d3d6cb244df39b59a9202bdc
Shantanu Gupta <shantanu.smg@gmail.com>
2020-05-12 15:47:19 -0700

    push messages with public s3 url for images

--- report_graffiti.py
+++ report_graffiti.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import pika
 import boto3
 import datetime
@@ -9,14 +8,16 @@ import json
 from botocore.exceptions import ClientError
 
 s3_client = boto3.client('s3')
+# s3_resource = boto3.resource('s3')
 bucket = "report-graffiti"
+
 datetoday = datetime.date.today()
 obj_path = datetoday.strftime('%Y/%m/%d/')
-graffiti_dir = os.path.expanduser("~/graffiti") # 'C://cic//tempe-graffiti//sagemaker-graffiti-images//testinference' 
+graffiti_dir = os.path.expanduser("~/graffiti") # 'C://cic//tempe-graffiti//sagemaker-graffiti-images//testinference' #
 
 # RabbitMQ Host
 credentials = pika.PlainCredentials('admin', '05rX20@qmR!',erase_on_connect=True)
-parameters = pika.ConnectionParameters( host='52.3.176.47', # elastic ip
+parameters = pika.ConnectionParameters( host='52.3.176.47', # aws elastic ip for ec2 instance hosting rabbitmq
                                        port=5672,
                                        virtual_host='/host1',
                                        socket_timeout=2,
@@ -26,16 +27,22 @@ parameters = pika.ConnectionParameters( host='52.3.176.47', # elastic ip
 
 def upload_graffiti(filename, key):
     try:
-        response = s3_client.upload_file(filename, bucket, key)
+
+        # object_acl = s3_resource.ObjectAcl(bucket,key)
+        # response = object_acl.put(ACL='public-read')
+        response = s3_client.upload_file(filename, bucket, key, ExtraArgs={'ACL':'public-read', 'ContentType':'image/jpeg'})
+        # bucket.upload_file(filename, key, ExtraArgs={'ACL':'public-read'})
     except ClientError as e:
         logging.error(e)
         return False
     return True
 def make_connection():
     connection = pika.BlockingConnection(parameters)
-    #Create a new channel with the next available channel number or pass in a channel number to use
+    # Create a new channel with the next available channel number or pass in a channel number to use
     ch = connection.channel()
-    #Declare queue, create if needed. This method creates or checks a queue. When creating a new queue the client can specify various properties that control the durability of the queue and its contents, and the level of sharing for the queue.
+    # Declare queue, create if needed. This method creates or checks a queue. 
+    # When creating a new queue the client can specify various properties that control the durability of the queue and its contents,
+    # and the level of sharing for the queue.
     ch.queue_declare(queue='graffiti-queue',durable=True)
     return ch
 
@@ -52,7 +59,7 @@ while True:
                 filename = os.path.join(graffiti_dir, image)
                 resp = upload_graffiti(filename, key)
                 if resp:
-                    image_s3_url = "https://{0}.s3.amazonaws.com/{1}".format(bucket,key)
+                    image_s3_url = ("https://{0}.s3.amazonaws.com/{1}".format(bucket,key)).replace(' ','+')
                     payload = { "image": image_s3_url,
                                 "geolocation": "33.4255104, -111.9400054" #sample
                                 }
@@ -62,16 +69,17 @@ while True:
                         try:
                             os.remove(filename)
                         except Exception as err:
-                            print(err,"\ncould not remove image from captured dir",image)
+                            logging.error(err,"\ncould not remove image from captured dir: ",image)
                     # Recover on all connection errors
                     except:
                         connected = False
                         continue
+                    # print(image_s3_url)
                     
         else:
             time.sleep(10)
             try:
-                print("Message Count",channel.queue_declare(queue="graffiti-queue", durable=True).method.message_count)
+                logging.info("Message Count ",channel.queue_declare(queue="graffiti-queue", durable=True).method.message_count)
             except pika.exceptions.UnroutableError as e:
                 connected = False
                 continue
