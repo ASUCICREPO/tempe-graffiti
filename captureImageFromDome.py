ee016c17d19e731d8392b49b1a222e8c5568c3b1
Sriprashanth Ramamoorthy <sramamo3@asurite.asu.edu>
2020-06-16 13:34:55 -0700

    Capture Image from Dome Camera

---
+++ captureImageFromDome.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import os
+import time
+
+capture = cv2.VideoCapture('rtsp://root:asucic2020@192.168.0.16:554/live.sdp')
+img_counter=0
+frame_rate = 5
+prev = 0
+while True:
+    time_elapsed = time.time() - prev
+    graffiti_dir = os.path.expanduser("~\\Projects\\tempe-graffiti\\graffiti") # 'C://cic//tempe-graffiti//sagemaker-graffiti-images//testinference' # 
+    ret, frame = capture.read()
+    if time_elapsed > 1./frame_rate:
+        prev = time.time()
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(os.path.join(graffiti_dir ,img_name), frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+
+        
+
+capture.release()
+cv2.destroyAllWindows()
